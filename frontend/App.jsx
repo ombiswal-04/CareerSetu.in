35514cbe886b5c9e56e3bf2cd3b2bd90ced496e2
Om Biswal <biswalom04@gmail.com>
2026-02-05 00:45:39 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -61,9 +61,9 @@ export default function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <BrowserRouter basename='/CareerSetu.in'>
+        <HashRouter>
           <AppLayout />
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </ThemeProvider>
   )
